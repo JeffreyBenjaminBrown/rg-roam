b846ac3915d1fd940c80dcc0946246bfd13944ee
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2025-06-06 13:55:38 -0500

    relax a constraint
      namely, that the first search term be the first term in the title

--- main.el
+++ main.el
@@ -17,7 +17,7 @@ Results are displayed in a `grep-mode' interactive buffer."
          (words (split-string terms "[[:space:]]+" t))
          ;; Build a flexible PCRE pattern that allows anything in between terms
          (pattern
-          (concat "^\\s*(:(ROAM_ALIASES:)|(#\\+title:)|(#\\+filetags:))\\s+"
+          (concat "^\\s*(:(ROAM_ALIASES:)|(#\\+title:)|(#\\+filetags:))\\s+.*"
                   (mapconcat (lambda (w) (regexp-quote w)) words ".*")))
          (cmd (format
                "rg -P --no-heading --line-number --color=never -e %S -g '*.org' --glob '!*.git'"
